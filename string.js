0257a88259a74bea254738cfb1bd5c8c7b38cc13
Shreya2004449 <shreyavns02006@gmail.com>
2025-04-29 18:50:20 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,16 @@
+let name ="john";
+let massage='hello';
+console.log(name+massage);
+console.log(massage.indexOf());
+
+let fruits1 ="apple";
+let friuts2= "banana";
+console.log(fruits1+friuts2);
+console.log(friuts2.length);
+console.log(fruits1.trim());
+console.log(friuts2.toUpperCase());
+console.log(fruits1.charAt(4));
+console.log(fruits1.indexOf("l"));
+console.log(friuts2.slice(0,6));
+console.log(fruits1.split(""));
+console.log(friuts2.toLowerCase());
